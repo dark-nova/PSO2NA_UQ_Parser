0079e2abfaba497790456fc8226c7cfc3f9edf08
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2020-05-25 15:25:14 -0700

    Add db to write records; add config module to handle db + logging

---
+++ config.py
@@ -0,0 +1,43 @@
+import logging
+import logging.handlers
+import sqlite3
+
+
+LOGGER = logging.getLogger('pso2_news')
+LOGGER.setLevel(logging.DEBUG)
+
+FH = logging.handlers.RotatingFileHandler(
+    'news.log',
+    maxBytes=4096,
+    backupCount=5,
+    )
+FH.setLevel(logging.DEBUG)
+
+CH = logging.StreamHandler()
+CH.setLevel(logging.INFO)
+
+FH.setFormatter(
+    logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+    )
+CH.setFormatter(
+    logging.Formatter(
+        '%(levelname)s - %(message)s'
+        )
+    )
+
+LOGGER.addHandler(FH)
+LOGGER.addHandler(CH)
+
+
+DB = sqlite3.Connection('news.db')
+CURSOR = DB.cursor()
+
+SCHEMA = {
+    'UQ': '(DATE TEXT, NAME TEXT)'
+    }
+
+for table, schema in SCHEMA.items():
+    CURSOR.execute('CREATE TABLE IF NOT EXISTS {0} {1}'.format(table, schema))
+    DB.commit()
--- uq.py
+++ uq.py
@@ -6,17 +6,18 @@ import requests
 from bs4 import BeautifulSoup, Tag
 from more_itertools import grouper
 
+import config
 
-example_main = 'example-urgent_quests.html'
-example_scheds = [
-    '2020-02',
-    '2020-03',
+
+EXAMPLE_MAIN = 'example-urgent_quests.html'
+EXAMPLE_SCHEDS = [
+    #'2020-02',
+    #'2020-03',
     '2020-05_3',
     'about',
     ]
 
-today = pendulum.today()
-
+TODAY = pendulum.today()
 
 MONTHS = [
     'January', 'February', 'March', 'April', 'May', 'June',
@@ -35,19 +36,13 @@ NOT_UQ = [
     ]
 
 
-def open_example_uq_sched(schedule: str) -> BeautifulSoup:
-    with open(f'example-urgent_quest-{schedule}.html', 'r') as example:
-        soup = BeautifulSoup(example, 'html.parser')
-    return soup
-
-
 def parse_date(month: int, day: int) -> Tuple[int, int, int]:
     """Parse a date given month and day only and convert to
     a tuple."""
-    if month < today.month:
-        year = today.year #+ 1
+    if month < TODAY.month:
+        year = TODAY.year #+ 1
     else:
-        year = today.year
+        year = TODAY.year
     return year, month, day
 
 
@@ -85,6 +80,19 @@ def is_not_uq(uq: str) -> bool:
     return uq in NOT_UQ
 
 
+def get_uq_from_tile(tile: Tag, colors: Dict[str, str]) -> Union[str, None]:
+    """Get a UQ from a tile given the tile's color."""
+    for attr in tile['style'].split(';'):
+        if not attr:
+            continue
+        a, value = attr.split(':')
+        if a == 'background':
+            try:
+                return colors[value]
+            except KeyError:
+                return
+
+
 def get_hex_color_from_tile(tile: Tag) -> str:
     """Get a HEX color from the tile background."""
     for attr in tile['style'].split(';'):
@@ -92,6 +100,7 @@ def get_hex_color_from_tile(tile: Tag) -> str:
             continue
         a, value = attr.split(':')
         if a == 'background':
+            print(value)
             rgb = '#' + ''.join(
                 [hex(int(n))[2:] for n in RGB.search(value).group(1, 2, 3)]
                 ).upper()
@@ -109,94 +118,112 @@ def get_colors_from_table(table: Tag) -> Dict[str, str]:
     return colors
 
 
-def get_uq_from_tile(tile: Tag, colors: Dict[str, str]) -> Union[str, None]:
-    """Get a UQ from a tile given the tile's color."""
-    for attr in tile['style'].split(';'):
-        if not attr:
-            continue
-        a, value = attr.split(':')
-        if a == 'background':
-            try:
-                return colors[value]
-            except KeyError:
-                return
+class UQSchedule:
+    """Represents a schedule page for Urgent Quests."""
 
+    def __init__(self, url_or_file: str, *, url: bool = True) -> None:
+        """Initialize the schedule parser with a URL or local file.
 
-def parse_only_tables(tables: List[Tag]) -> Dict[pendulum.datetime, str]:
-    """Parse a table, ignoring any color code. Used in 2020-02.
+        Args:
+            url_or_file (str): the URL or local file of a UQ schedule
+            url (bool, optional): is url_or_file a URL?
+                defaults to None
 
-    Args:
-        tables (List[Tag]): a list of html tables; tables here are
-            2-columns wide, with time ranges in column 1 and UQ name
-            in column 2
-
-    Returns:
-        Dict[pendulum.datetime, str]: a dictionary with keys that
-            correspond to time and values being the UQ name at the time
-
-    """
-    schedule = {}
-    for table in tables:
-        rows = table.find_all('tr')
-        cols = rows[0].find_all('td')
-        # Special case: 2nd table of example-urgent_quest-2020-02.html
-        if len(cols) == 1:
-            rows.pop(0)
-            cols = rows[0].find_all('td')
-        year, month, day = parse_special_date(
-            table.previous_sibling.text
-            )
-        for row in rows[1:]:
-            time, uq = [tag.text.strip() for tag in row.find_all('td')]
-            # Special case:
-            #   2nd table of example-urgent_quest-2020-02.html
-            if time == 'Time (PST)':
-                continue
-            if is_not_uq(uq):
-                continue
-            hour, minute = parse_time_range(time)
-            dt = pendulum.datetime(year, month, day, hour, minute)
-            schedule[dt] = uq
-    return schedule
-
-
-def parse_uq_sched(soup: BeautifulSoup):
-    schedule = {}
-    tables = soup.find('div', 'emergency cms')
-    for table_a, table_b in grouper(tables.find_all('table'), 2):
-        rows = table_a.find_all('tr')
-        cols = rows[0].find_all('td')
-        if len(cols) == 1:
-            rows.pop(0)
-            cols = rows[0].find_all('td')
-        # Special case: example-urgent_quest-2020-02.html
-        if len(cols) == 2:
-            # Because 2020-02 does not have a color code per table,
-            # iterate over both tables instead.
-            return parse_only_tables([table_a, table_b])
+        """
+        if url:
+            page = requests.get(url)
+            self.soup = BeautifulSoup(page.text, 'html.parser')
         else:
-            dates = [
-                parse_date(*[int(n) for n in col.text.split('/')])
-                for col in cols[1:]
-                ]
-            # Skip the 2nd row (days of the week) and 3rd row ("Time (PDT)").
-            color_map = get_colors_from_table(table_b)
-            for row in rows[3:]:
-                time = parse_time(row.find('td').text)
-                for i, tile in enumerate(row.find_all('td')[1:]):
-                    uq = get_uq_from_tile(tile, color_map)
-                    if not uq:
-                        continue
-                    schedule[
-                        pendulum.datetime(
-                            *(dates[i] + time), tz='America/Los_Angeles'
-                            )
-                        ] = uq
+            with open(f'example-urgent_quest-{schedule}.html', 'r') as example:
+                self.soup = BeautifulSoup(example, 'html.parser')
 
-    return schedule
+
+    def parse(self) -> None:
+        """Parse the page and convert into database entries."""
+        self.schedule = {}
+        tables = self.soup.find('div', 'emergency cms')
+        for table_a, table_b in grouper(tables.find_all('table'), 2):
+            rows = table_a.find_all('tr')
+            cols = rows[0].find_all('td')
+            if len(cols) == 1:
+                rows.pop(0)
+                cols = rows[0].find_all('td')
+            # Special case: example-urgent_quest-2020-02.html
+            if len(cols) == 2:
+                # Because 2020-02 does not have a color code per table,
+                # iterate over both tables instead.
+                self.parse_only_tables([table_a, table_b])
+            else:
+                dates = [
+                    parse_date(*[int(n) for n in col.text.split('/')])
+                    for col in cols[1:]
+                    ]
+                # Skip row 2 (days of the week) and row 3 ("Time (PDT)").
+                color_map = get_colors_from_table(table_b)
+                for row in rows[3:]:
+                    time = parse_time(row.find('td').text)
+                    for i, tile in enumerate(row.find_all('td')[1:]):
+                        uq = get_uq_from_tile(tile, color_map)
+                        if not uq:
+                            continue
+                        self.schedule[
+                            pendulum.datetime(
+                                *(dates[i] + time), tz='America/Los_Angeles'
+                                )
+                            ] = uq
+
+        self.write_to_db()
+
+    def parse_only_tables(self, tables: List[Tag]) -> None:
+        """Parse a table, ignoring any color code. Used in 2020-02.
+
+        Args:
+            tables (List[Tag]): a list of html tables; tables here are
+                2-columns wide, with time ranges in column 1 and UQ name
+                in column 2
+
+        """
+        for table in tables:
+            rows = table.find_all('tr')
+            cols = rows[0].find_all('td')
+            # Special case: 2nd table of example-urgent_quest-2020-02.html
+            if len(cols) == 1:
+                rows.pop(0)
+                cols = rows[0].find_all('td')
+            year, month, day = parse_special_date(
+                table.previous_sibling.text
+                )
+            for row in rows[1:]:
+                time, uq = [tag.text.strip() for tag in row.find_all('td')]
+                # Special case:
+                #   2nd table of example-urgent_quest-2020-02.html
+                if time == 'Time (PST)':
+                    continue
+                if is_not_uq(uq):
+                    continue
+                hour, minute = parse_time_range(time)
+                dt = pendulum.datetime(year, month, day, hour, minute)
+                self.schedule[dt] = uq
+
+    def write_to_db(self) -> None:
+        """Write the schedule to DB."""
+        config.CURSOR.execute('SELECT DATE FROM UQ')
+        dates = config.CURSOR.fetchall()
+        for date, uq in self.schedule.items():
+            if date in dates:
+                config.LOGGER.info(
+                    f'{date} (UQ: {uq}) was found in the DB! Skipped.'
+                    )
+                continue
+            config.CURSOR.execute(
+                'INSERT INTO UQ VALUES (?, ?)',
+                (str(date), uq)
+                )
+        config.DB.commit()
+
 
 
 if __name__ == '__main__':
-    for schedule in example_scheds:
-        soup = open_example_uq_sched(schedule)
-        print(parse_uq_sched(soup))
+    for schedule in EXAMPLE_SCHEDS:
+        s = UQSchedule(schedule, url=False)
+        s.parse()
